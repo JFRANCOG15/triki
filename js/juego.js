f8f9b86794c1af912d3183a6cb38b296b7140b33
jhoan franco <sebastianfrancogarces1322@gmail.com>
2026-05-29 09:44:28 -0500

    se crea funciones

--- js/juego.js
+++ js/juego.js
@@ -1,11 +1,23 @@
 let turno ='O'
 impTablero()
 
+function turnoAleatorio(){
+    let turno = Math.floor(Math.random() * (2 - 1 + 1) + 1)
+    if (turno == 1) {
+        return turno = '1'
+    } else {
+        return turno = '2'
+    }
+}
+
+
 function jugada(id_Celda){
 
     let celda = document.getElementById(id_Celda)
-    celda.innerText = turno
-    turno = cambiaTurno(turno)
+    if (celda.innerText == ''){
+        celda.innerText = turno
+        turno = cambiaTurno(turno)
+    }
 }
 
 function impTablero(turno){
